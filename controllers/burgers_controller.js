3ace60b2da61e3104d54698aedf0d46a496d161a
Priyam <priyam009@live.com>
2019-09-17 23:32:59 +1000

    Add route to get all data and render to index handlebar

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+//Import burger.js model to use its database function
+var burger = require('../models/burger.js');
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var burgers_hbs = {
+      burgers: data
+    };
+    console.log("burgers_hbs", burgers_hbs);
+    res.render("index", burgers_hbs);
+  })
+})
