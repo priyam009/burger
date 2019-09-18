836cfcb202c3011ac7f47bcae6bcd170d60e4210
Priyam <priyam009@live.com>
2019-09-18 21:33:28 +1000

    Update routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //Import burger.js model to use its database function
 var burger = require('../models/burger.js');
 
-router.get("/", function(req, res) {
+router.get("/api", function(req, res) {
   burger.selectAll(function(data) {
     
     console.log("data", data);
