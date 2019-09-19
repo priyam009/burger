1b0a7b9a2b002977b371ee7fb649eda15f315b6a
Priyam <priyam009@live.com>
2019-09-19 16:50:09 +1000

    Add post route for new burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,9 +7,15 @@ var burger = require('../models/burger.js');
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     
-    console.log("data", data);
+    // console.log("data", data);
     res.render("index", {data: data});
   })
 });
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne("burger_name", [req.body.name], function(result) {
+    res.json({ id: result.insertId });
+  });
+})
 
 module.exports = router;
