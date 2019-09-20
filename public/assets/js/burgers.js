1b603dd77705bfef603082254b844e1aa467d783
Priyam <priyam009@live.com>
2019-09-20 19:58:59 +1000

    Add functionality to update the burger from 'the list' to 'devoured' when devoured button pressed

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+$(function() {
+
+  //To add new burger to the list
   $("#submit").on("click", function() {
     event.preventDefault();
 
@@ -8,8 +10,7 @@ $(document).ready(function() {
         .trim()
     };
 
-    console.log("newBurger", newBurger);
-
+    //Send POST request
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
@@ -18,4 +19,30 @@ $(document).ready(function() {
       location.reload();
     });
   });
+
+  //To update the burger to devoured list
+  $(".add-burger").on("click", function(event) {
+    event.preventDefault();
+
+    var id= $(this).data("id");
+    var devoured= $(this).data("devour");
+
+    console.log("id", id);
+    console.log("devoured", devoured);
+
+    var newState = {
+      devoured: devoured
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newState
+    }).then(function(result) {
+      console.log("Burger Devoured!!");
+      console.log(result);
+      location.reload();
+    })
+  })
+
+
 });
