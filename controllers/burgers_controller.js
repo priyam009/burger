f5fb2849076cda557fbdd6fbb14da2735ff0be99
Priyam <priyam009@live.com>
2019-09-20 19:56:47 +1000

    Add route to update devoured (PUT)

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,12 @@ var burger = require('../models/burger.js');
 
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
+    var hbsObj = {
+      burgers: data
+    };
     
     // console.log("data", data);
-    res.render("index", {data: data});
+    res.render("index", hbsObj);
   })
 });
 
@@ -16,6 +19,21 @@ router.post("/api/burgers", function(req, res) {
   burger.insertOne("burger_name", [req.body.name], function(result) {
     res.json({ id: result.insertId });
   });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  var cols = "devoured = " + req.body.devoured;
+
+  burger.updateOne(cols, condition, function(result) {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  })
 })
+
+
 
 module.exports = router;
